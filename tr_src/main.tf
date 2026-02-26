a96e758cf1b2186cab72be7b956277d5692f1da9
tatekitzhak <tatekosi@gmail.com>
2026-02-26 13:35:50 -0800

    Fix error

--- tr_src/main.tf
+++ tr_src/main.tf
@@ -52,12 +52,6 @@ output "caller_user_id" {
   value = data.aws_caller_identity.current.user_id
 }
 
-output "ec2_instance_id" {
-  description = "EC2 instance ID for CD deploy (SSM); use in workflow or secret EC2_INSTANCE_ID"
-  value       = module.ec2.ec2_instance_id
-}
-
-
 
 
 resource "null_resource" "create_file_localy" {
